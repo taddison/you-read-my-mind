3bd7872c824995ffcadf22bf717c870afb694c40
Tim Addison <git@tjaddison.com>
2020-03-29 11:37:46 -0400

    game -> index

--- pages/index.js
+++ pages/index.js
@@ -1,203 +1,90 @@
-import Head from 'next/head'
+import React from "react";
+import useSWR, { mutate } from "swr";
+import cookie, { serialize } from "cookie";
+import { CookieNames } from "../consts";
+const crypto = require("crypto");
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const fetcher = (...args) => {
+  return fetch(...args).then(res => res.json());
+};
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+const Game = ({ sessionId }) => {
+  const { data: gameState, error } = useSWR("/api/getGameState", fetcher);
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
+  return (
+    <>
+      <header>
+        <h1>You Read My Mind</h1>
+      </header>
+      <main>
+        <div>
+          Players
+          <ul>
+            {gameState &&
+              gameState.players.map(player => {
+                return (
+                  <li key={player.sessionId}>
+                    {player.name}{" "}
+                    {player.sessionId === sessionId ? "(You)" : ""}
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+        <div>
+          <button
+            onClick={async () => {
+              await fetch("/api/removePlayer", {
+                method: "POST",
+              });
+              mutate("/api/getGameState");
+            }}
+          >
+            Remove Me
+          </button>
+          <button
+            onClick={async () => {
+              await fetch("/api/addPlayer", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ name: "The mighty" })
+              });
+              mutate("/api/getGameState");
+            }}
+          >
+            Add Me
+          </button>
+        </div>
+      </main>
+      <footer>
+        <small>SessionId: {sessionId}</small>
+      </footer>
+    </>
+  );
+};
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+export async function getServerSideProps(context) {
+  const cookies = cookie.parse(context.req.headers.cookie ?? "");
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+  let sessionId = cookies[CookieNames.Session];
 
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
+  if (!sessionId) {
+    sessionId = crypto.randomBytes(16).toString("hex");
 
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
-    </main>
+    // expire in 1 week
+    const expires = new Date(new Date().getTime() + 604800000);
 
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
+    context.res.setHeader(
+      "Set-Cookie",
+      serialize(CookieNames.Session, sessionId, { expires })
+    );
+  }
 
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
+  return {
+    props: { sessionId: sessionId }
+  };
+}
 
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-)
-
-export default Home
+export default Game;
