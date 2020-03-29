cc884dfd2a6922c9900ac4858b13fb4e6a1f72e2
Tim Addison <git@tjaddison.com>
2020-03-29 19:43:36 -0400

    only show players when there are players

--- components/playerList.js
+++ components/playerList.js
@@ -1,18 +1,24 @@
 import React from "react";
 
 const PlayerList = ({ playerList = [] }) => {
+  const otherPlayers = playerList.filter(p => !p.isPsychic && !p.isGuesser);
+
   return (
     <div>
       <ul>
         <li>Psychic: {playerList.find(p => p.isPsychic)?.name ?? "No psychic"}</li>
         <li>Guesser: {playerList.find(p => p.isGuesser)?.name ?? "No guesser"}</li>
       </ul>
+      {otherPlayers.length ? (
+        <div>
       Players
       <ul>
         {playerList.filter(p => !p.isPsychic && !p.isGuesser).map(player => {
           return <li key={player.sessionId}>{player.name}</li>;
         })}
       </ul>
+        </div>
+      ) : ''}
     </div>
   );
 };
