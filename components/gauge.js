e87b3f46c4f71d1c0c2a33004a2204dc3f7016f2
Tim Addison <git@tjaddison.com>
2020-04-11 12:57:48 -0400

    example layout for gauge

--- components/gauge.js
+++ components/gauge.js
@@ -8,7 +8,7 @@ const Gauge = ({width="24em", value = 60, min = 0, max = 100 }) => {
     .outerRadius(1)
     .startAngle(-Math.PI / 2)
     .endAngle(Math.PI / 2)
-    .cornerRadius(1)();
+    ();
 
   const percentScale = scaleLinear().domain([min, max]).range([0, 1]);
   const percent = percentScale(value);
@@ -25,7 +25,7 @@ const Gauge = ({width="24em", value = 60, min = 0, max = 100 }) => {
     .outerRadius(1)
     .startAngle(-Math.PI / 2)
     .endAngle(angle)
-    .cornerRadius(1)();
+    ();
 
   const colorScale = scaleLinear().domain([0, 1]).range(["#dbdbe7", "#4834d4"]);
 
@@ -76,7 +76,8 @@ const Gauge = ({width="24em", value = 60, min = 0, max = 100 }) => {
           d="M0.136364 0.0290102C0.158279 -0.0096701 0.219156 -0.00967009 0.241071 0.0290102C0.297078 0.120023 0.375 0.263367 0.375 0.324801C0.375 0.422639 0.292208 0.5 0.1875 0.5C0.0852272 0.5 -1.8346e-08 0.422639 -9.79274e-09 0.324801C0.00243506 0.263367 0.0803571 0.120023 0.136364 0.0290102ZM0.1875 0.381684C0.221591 0.381684 0.248377 0.356655 0.248377 0.324801C0.248377 0.292947 0.221591 0.267918 0.1875 0.267918C0.153409 0.267918 0.126623 0.292947 0.126623 0.324801C0.126623 0.356655 0.155844 0.381684 0.1875 0.381684Z"
           transform={`rotate(${
             angle * (180 / Math.PI)
-          }) translate(-0.2, -0.33)`}
+          }) translate(-0.2, -0.33)
+          `}
           fill="#6a6a85"
         />
       </svg>
--- pages/componentTest.js
+++ pages/componentTest.js
@@ -8,19 +8,32 @@ const ComponentTest = () => {
   const [value, setValue] = useState(0);
 
   return (
-    <div className="max-w-4xl mx-auto flex items-center mt-4 flex flex-col">
-      <Gauge min={min} max={max} value={value} />
-      <div
-        className="mt-6 text-5xl font-black"
-        style={{
-          fontFeatureSettings: "'zero', 'tnum' 1",
-        }}
-      >
-        {value}
+    <div className="max-w-6xl mx-auto mt-4">
+      <div className="flex items-center flex flex-col">
+        <div className="text-xl font-semibold">
+          Lucky
+        </div>
+        <div className="flex mt-6">
+          <div className="flex items-center italic mr-6">A Cat's Name</div>
+          <div className="flex flex-col items-center">
+            <Gauge min={min} max={max} value={value} />
+            <div
+              className="mt-6 text-5xl font-black"
+              style={{
+                fontFeatureSettings: "'zero', 'tnum' 1",
+              }}
+            >
+              {value}
+            </div>
+            <div className="w-3/4">
+              <Slider min={min} max={max} value={value} onChange={setValue} />
+            </div>
+          </div>
+          <div className="flex items-center italic ml-6">A Dog's Name</div>
+        </div>
       </div>
-      <Slider min={min} max={max} value={value} onChange={setValue} />
     </div>
-  )
-}
+  );
+};
 
 export default ComponentTest;
