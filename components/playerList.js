08f0be1794b3e27dca662456bd32ad181423aa8b
celinechole <github@celinechole.com>
2020-04-12 14:08:54 -0400

    more style

--- components/playerList.js
+++ components/playerList.js
@@ -6,18 +6,18 @@ const PlayerList = ({ playerList = [] }) => {
   const otherPlayers = playerList.filter((p) => !p.isPsychic && !p.isGuesser);
 
   return (
-    <div>
+    <div className="max-w-lg mx-2">
       <ul>
-        <li className="bg-gray-200 max-w-lg pb-2 mb-4">
+        <li className="bg-gray-200 pb-2 mb-4">
           <p className="bg-gray-400 py-1 px-2 rounded mb-2 font-semibold">
             Psychic
           </p>
           <p className="px-2">
             {playerList.find((p) => p.isPsychic)?.name ??
-              "Waiting for a psychic..."}
+              "Waiting for a psychic"}
           </p>
         </li>
-        <li className="bg-gray-200 max-w-lg pb-2">
+        <li className="bg-gray-200 pb-2">
           <p className="bg-gray-400 py-1 px-2 rounded mb-2 font-semibold">
             Guesser
           </p>
@@ -28,9 +28,11 @@ const PlayerList = ({ playerList = [] }) => {
         </li>
       </ul>
       {otherPlayers.length ? (
-        <div className="mt-4">
-          Players
-          <ul>
+        <div className="mt-4 bg-gray-200">
+        <p className="bg-gray-400 py-1 px-2 rounded mb-2 font-semibold">
+            Players
+          </p>
+          <ul className="bg-gray-200 pb-2 px-2">
             {playerList
               .filter((p) => !p.isPsychic && !p.isGuesser)
               .map((player) => {
