6582c990385f6c93fde12a46bc73ec8b9f91d104
Tim Addison <git@tjaddison.com>
2020-04-22 17:14:20 -0400

    make index static

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import React from "react";
-import cookie, { serialize } from "cookie";
-import { CookieNames } from "../consts";
 
-const Home = ({ sessionId }) => {
+const Home = () => {
   return (
     <div className="min-h-screen flex flex-col text-gray-700">
       <header className="bg-gray-200 p-4">
@@ -14,32 +12,10 @@ const Home = ({ sessionId }) => {
         Join a game (/game/gameId)
       </main>
       <footer className="bg-gray-200 p-1">
-        <small>SessionId: {sessionId}</small>
+        <small>This page is currently static</small>
       </footer>
     </div>
   );
 };
-
-export async function getServerSideProps(context) {
-  const cookies = cookie.parse(context.req.headers.cookie ?? "");
-
-  let sessionId = cookies[CookieNames.Session];
-
-  if (!sessionId) {
-    sessionId = crypto.randomBytes(16).toString("hex");
-
-    // expire in 1 week
-    const expires = new Date(new Date().getTime() + 604800000);
-
-    context.res.setHeader(
-      "Set-Cookie",
-      serialize(CookieNames.Session, sessionId, { expires })
-    );
-  }
-
-  return {
-    props: { sessionId: sessionId },
-  };
-}
 
 export default Home;
